83d8ca18df8fe853b9d8547779043010b77033fa
Cameron Dutro <camertron@gmail.com>
2017-12-06 15:22:34 -0800

    Oops, remove tester

--- tester.rb
+++
@@ -1,8 +0,0 @@
-require 'bundler'
-require 'prebundler'
-require 'pry-byebug'
-
-# gems = Prebundler::GemfileInterpreter.interpret('./Gemfile', '/bundle').gems
-gf = Prebundler::GemfileInterpreter.interpret('/Users/cameron/workspace/lumos_rails/Gemfile', '/Users/cameron/.rbenv/versions/2.4.2/lib/ruby/gems/2.4.0')
-binding.pry
-puts 'foo'
