029986b173a46c3f2e252042ab9ed522f5f008cf
Cameron Dutro <camertron@gmail.com>
2021-12-31 21:26:59 -0800

    Skip directories

--- lib/prebundler/gem_ref.rb
+++ lib/prebundler/gem_ref.rb
@@ -195,6 +195,8 @@ module Prebundler
 
     def add_files_to_tar(tar_io, files)
       files.each do |file|
+        next if File.directory?(file)
+
         mode = File.stat(file).mode
         tar_io.add_file(file, mode) do |new_file|
           new_file.write(File.binread(file))
